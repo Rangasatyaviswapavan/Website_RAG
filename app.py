3093d1dba0565afbf33b7aef2c6055e4b4bd2334
Satya Viswa Pavan Ranga <pavan.sv.ranga@gmail.com>
2024-02-18 05:11:54 -0500

    completed creating database and moved to seperate file

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from langchain_core.messages import AIMessage, HumanMessage
+from create_data import get_data_from_web
+from dotenv import load_dotenv
+load_dotenv()
 
 def get_answer(query):
     return "give me money$$"
+
 
 st.set_page_config(page_title="Website RAG",page_icon="🪦")
 st.title("Chat with websites!!")
@@ -19,6 +23,9 @@ with st.sidebar:
 if url is None  or url =="" or key is None or key =="":
     st.info("Please Enter Details!!")
 else:
+    docs= get_data_from_web(url)
+    with st.sidebar:
+        st.write(docs)
     query = st.chat_input("Ask Website RAG...")
     if query is not None and query!="":
         answer= get_answer(query)
